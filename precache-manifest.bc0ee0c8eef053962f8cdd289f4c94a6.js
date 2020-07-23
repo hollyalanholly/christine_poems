b268263de14bfa2e5ca58ee62763d1de1b2a664f
Holly <hollyduckworth@hotmail.co.uk>
2020-07-23 18:25:11 +0100

    Updates

---
+++ precache-manifest.bc0ee0c8eef053962f8cdd289f4c94a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d54f1f8f17e35c47253a1ce81b7d2e",
+    "url": "/christine_poems/index.html"
+  },
+  {
+    "revision": "4734adaf402b629ceddf",
+    "url": "/christine_poems/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "63a72ee12aca83c4e811",
+    "url": "/christine_poems/static/css/main.9b091390.chunk.css"
+  },
+  {
+    "revision": "4734adaf402b629ceddf",
+    "url": "/christine_poems/static/js/2.657b65ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/christine_poems/static/js/2.657b65ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a72ee12aca83c4e811",
+    "url": "/christine_poems/static/js/main.87ca9fcf.chunk.js"
+  },
+  {
+    "revision": "b507834dbac83c932b00",
+    "url": "/christine_poems/static/js/runtime-main.01953c76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/christine_poems/static/media/logo.5d5d9eef.svg"
+  }
+]);
