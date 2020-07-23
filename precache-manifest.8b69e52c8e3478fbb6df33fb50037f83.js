f0bb8132c943105984015ed5b96f6422c5f7ed51
Holly <hollyduckworth@hotmail.co.uk>
2020-07-23 20:17:57 +0100

    Updates

---
+++ precache-manifest.8b69e52c8e3478fbb6df33fb50037f83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df306847280d9832550e74c32a497fa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d94728eb1598616a653",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9d2e5a2d803b71c67ffb",
+    "url": "/static/css/main.9b091390.chunk.css"
+  },
+  {
+    "revision": "8d94728eb1598616a653",
+    "url": "/static/js/2.ad14804a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad14804a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2e5a2d803b71c67ffb",
+    "url": "/static/js/main.fdff8f45.chunk.js"
+  },
+  {
+    "revision": "606497defb50fd1bb040",
+    "url": "/static/js/runtime-main.278b9ff2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/christine_poems/precache-manifest.777789b75df3e9ab80cfba82ce0eb622.js"
+  "/precache-manifest.8b69e52c8e3478fbb6df33fb50037f83.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/christine_poems/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
