07844e77c602cfd12d15d724d35a6677b21aee38
Holly <hollyduckworth@hotmail.co.uk>
2020-07-23 19:21:26 +0100

    cant get website to show

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import {
+  HashRouter,
+  Link,
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
@@ -10,17 +12,26 @@ import HomePage from './Pages/HomePage';
 import ContactMe from './Pages/ContactMe';
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <Router>
-          <div className ="App">
-            <Route path="/" component={HomePage} exact/>
-            <Route path="/contact_me" component={ContactMe} />
-          </div>
+      <HashRouter basename='/'>
+        <div className="App">
 
-      </Router>
+          <ul>
+            <li><Link to="/">HomePage</Link></li>
+            <li><Link to="contact_me">ContactMe</Link></li>
+          </ul>
+          <hr />
+
+          <Route path="/" component={HomePage} exact />
+          <Route path="/contact_me" component={ContactMe} />
+        </div>
+      </HashRouter>
     );
   }
 }
+
+// const HomePage = () => <div><h2>HomePage</h2></div>
+// const ContactMe = () => <div><h2>ContactMe</h2></div>
 
 export default App;
